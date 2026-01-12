cd848b7602f9ea02e3a55800c34d6a9e76defc22
Huong <vothihuong231101@gmail.com>
2026-01-12 22:16:08 +0700

    add container from prd image to prd accordion

---
+++ src/ProductInformation/ProductInformation.tsx
@@ -0,0 +1,8 @@
+export const ProductInformation = () => {
+  <section id="product-info py-[32px]">
+    <div className="page-width px-[1.5rem]">
+        {/* các section bên trong từ image đến product accordion */}
+
+    </div>
+  </section>;
+};
