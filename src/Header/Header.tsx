d157f9d1731f45e43f13c78edb4ed20ef06be6ce
Huong <vothihuong231101@gmail.com>
2026-01-12 22:15:22 +0700

    add Header

---
+++ src/Header/Header.tsx
@@ -0,0 +1,12 @@
+export const Header = () => {
+  <header className="h-[70px]">
+    <div className="desktop"></div>
+    <div className="mobile p-[12px]">
+      <img
+        src="images/logo.avif"
+        alt=""
+        className="heder__logo max-w-[160px] mx-auto"
+      />
+    </div>
+  </header>;
+};
