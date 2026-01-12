3c7a8edcc58cd995c0ed1f33760f1196e7a48fc9
Huong <vothihuong231101@gmail.com>
2026-01-12 22:36:40 +0700

    product elements

---
+++ src/ProductAccordion/ProductAccordion.tsx
@@ -0,0 +1,94 @@
+export const ProductAccordion = () => {
+  <div className="product__accordion mt-[16px]">
+    <div className="product_tab-block mb-[16px] p-[16px] rounded-[32px] border border-[#d2d2d2]">
+      <div className="product_tab-thumb flex justify-between items-center gap-[12px]">
+        <p className="text-[16px]">Ingredients proven by science</p>
+        <span className="w-[24px]">
+          <img src="https://cdn.shopify.com/s/files/1/0917/5649/5191/files/Button_To_Expand_1_8a6027c0-281a-4c6b-8881-60673c7255ec.png?v=1758716844" />
+        </span>
+      </div>
+      <div className="text-[16px] hidden hidden text-[16px]">
+        <p>
+          <strong>Sculptique Ingredients:</strong>
+        </p>
+        <ul>
+          <li>
+            <strong>Echinacea purpurea Extract</strong> – Known for its
+            anti-inflammatory properties, it may support skin health.
+          </li>
+          <li>
+            <strong>Dandelion Extract</strong> – Traditionally used as a
+            diuretic, it may help reduce water retention.
+            <br />
+            <strong>Burdock Powder</strong>– Contains antioxidants that may
+            promote skin clarity.
+          </li>
+          <li>
+            <strong>Cleavers Extract</strong> – Believed to support lymphatic
+            drainage and detoxification.
+          </li>
+          <li>
+            <strong>Rutin</strong> – A flavonoid that may strengthen blood
+            vessels and improve circulation.
+          </li>
+          <li>
+            <strong>Bromelain Powder</strong> – An enzyme from pineapple that
+            may reduce inflammation and support tissue repair.
+          </li>
+          <li>
+            <strong>Lemon Powder</strong> – Rich in vitamin C, it may aid in
+            collagen production and skin rejuvenation.
+          </li>
+          <li>
+            <strong>Kelp Extract</strong> – A source of iodine and minerals that
+            may support skin metabolism.
+          </li>
+        </ul>
+        <p>
+          These natural ingredients work together to reduce puffiness, bloating,
+          fluid retention.
+        </p>
+      </div>
+    </div>
+
+    <div className="product_tab-block mb-[16px] p-[16px] rounded-[32px] border border-[#d2d2d2]">
+      <div className="product_tab-thumb flex justify-between items-center gap-[12px]">
+        <p className="text-[16px]">How does it actually work?</p>
+        <span className="w-[24px]">
+          <img src="https://cdn.shopify.com/s/files/1/0917/5649/5191/files/Button_To_Expand_1_8a6027c0-281a-4c6b-8881-60673c7255ec.png?v=1758716844" />
+        </span>
+      </div>
+      <div className="text-[16px] hidden hidden text-[16px]">
+        <p>
+          Sculptique works by improving blood flow and supporting lymphatic
+          drainage to reduce fluid buildup that causes puffiness, inflammation,
+          and water retention. It also reduces inflammation and boosts collagen
+          production to help skin become firmer and smoother.
+        </p>
+      </div>
+    </div>
+
+    <div className="product_tab-block mb-[16px] p-[16px] rounded-[32px] border border-[#d2d2d2] active">
+      <div className="product_tab-thumb flex justify-between items-center gap-[12px]">
+        <p className="text-[16px]">Shipping and returns</p>
+        <span className="w-[24px]">
+          <img src="https://cdn.shopify.com/s/files/1/0917/5649/5191/files/Button_To_Expand_1_8a6027c0-281a-4c6b-8881-60673c7255ec.png?v=1758716844" />
+        </span>
+      </div>
+      <div className="product_tab-content pt-[16px] text-[16px] hidden">
+        <p>
+          All of Sculptique orders get FREE shipping straight from our USA
+          warehouse. Orders are usually shipped out within 1-2 working days, and
+          you should receive the order within 3-7 working days for domestic USA
+          orders, and within 10 working days for International orders.
+        </p>
+        <p>
+          We also offer a 60-day money back guarantee - if you are unsatisfied
+          with our product, you can take advantage of our guarantee and ship
+          back the product to us to get your return within 60 days of receiving
+          your order.
+        </p>
+      </div>
+    </div>
+  </div>;
+};
---
+++ src/ProductReviews/ProductReviews.tsx
@@ -0,0 +1,36 @@
+export const ProductReivews = () => {
+  <div className="review bg-(--yellow-background) mt-[24px] p-[12px] rounded-[12px]">
+    <div className="review__head flex gap-[16px] mb-[8px]">
+      <div className="review__head--avatar w-[15%]">
+        <img src="images/review-owner.webp" alt="" />
+      </div>
+
+      <div className="review__head--details w-[100%]">
+        <p className="details__name text-[16px] flex items-center gap-[8px] leading-[1.3em]">
+          Margaret Ellison | FL
+          <span className="flex items-center text-[16px] max-w-[120px]">
+            <img src="images/verified.avif" alt="" />
+          </span>
+        </p>
+
+        <div className="stars max-w-[100px]">
+          <img src="images/stars-review.png" alt="" />
+        </div>
+      </div>
+    </div>
+    <div className="review-description text-[16px] font-(family-name:--body-font)">
+      <p className="mb-[8px] font-medium">
+        <strong>Even my husband noticed... the spark came back.</strong>
+      </p>
+      <p>
+        After years of hiding under coverups, picking apart my body, and feeling
+        disconnected, I finally feel at ease again. I feel less bloated,
+        lighter, like my body is working with me not against me. These past
+        months, I’ve been present. Even my marriage feels renewed, not just in
+        how I look but in how I feel. It’s hard to explain, but once you try it
+        you’ll get it. I’d recommend this to my closest friends without
+        question.
+      </p>
+    </div>
+  </div>;
+};
