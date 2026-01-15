7ca9272e9f1deebadd2a854494bc310a74b5bfa5
Huong <vothihuong231101@gmail.com>
2026-01-15 10:23:54 +0700

    fix bug while build

--- src/components/ProductAccordion/ProductAccordion.tsx
+++ src/components/ProductAccordion/ProductAccordion.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
 
 export const ProductAccordion = () => {
   const [openedAccordion, setOpenedAccordion] = useState<number>(-1);
